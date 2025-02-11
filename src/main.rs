23fec0aac55aac29743f2925852afb9dbd357f92
Yudistira <yudistira.sd2@gmail.com>
2025-02-11 09:40:57 +0700

    rust loop & loop with label

--- src/main.rs
+++ src/main.rs
@@ -407,3 +407,57 @@ fn if_elseif_expression_using_automatic_let() {
 
     println!("{}", result);
 }
+
+#[test]
+fn loop_expression() {
+    let mut counter = 0;
+
+    loop {
+        counter += 1;
+
+        if counter > 100 {
+            break;
+        } else if counter % 2 == 0 {
+            continue;
+        }
+
+        println!("Counter : {}", counter);
+    }
+}
+
+#[test]
+fn loop_return_value() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter > 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("{}", result)
+}
+
+#[test]
+fn nested_loop_with_label() {
+    let mut number = 1;
+
+    'first_loop: loop {
+        let mut i = 1;
+
+        loop {
+            if number > 10 {
+                break 'first_loop;
+            }
+
+            println!("{} x {} = {}", number, i, number * i);
+            i += 1;
+            if i > 10 {
+                break;
+            }
+        }
+
+        number += 1;
+    }
+}
